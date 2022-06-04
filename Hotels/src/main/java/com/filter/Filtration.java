0329bdfff93ed3220d330782ae29ae12e99b45f8
Nik_Gribok <mushroomicus@gmail.com>
2022-06-04 14:38:05 +0300

    fix filter condition from city to state

--- Hotels/src/main/java/com/filter/Filtration.java
+++ Hotels/src/main/java/com/filter/Filtration.java
@@ -1,22 +1,20 @@
 package com.filter;
 
 import com.convertation.ConvertXMLInObjects;
-import com.convertation.RunReadXML;
+import com.readingXml.ReadXML;
 import com.models.Hotel;
-import com.models.Hotels;
-import com.sun.org.apache.xerces.internal.util.SynchronizedSymbolTable;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Filtration {
     public static void main(String[] args) {
-        RunReadXML readXML = new RunReadXML();
+        ReadXML readXML = new ReadXML();
         ConvertXMLInObjects convertXMLInObjects = new ConvertXMLInObjects();
         List<Hotel> listOfHotels = readXML.readingXml();
         List<Hotel> hotels = listOfHotels.stream().filter(hotel -> hotel.getName().contains("Hilton") &&
-                (hotel.getAddress().getCity().toUpperCase().contains("NEW YORK") ||
-                        hotel.getAddress().getCity().toUpperCase().contains("NY"))).collect(Collectors.toList());
+                (hotel.getAddress().getState().toUpperCase().contains("NEW YORK") ||
+                        hotel.getAddress().getState().toUpperCase().contains("NY"))).collect(Collectors.toList());
 //        System.out.println(hotels);
         hotels.forEach(System.out::println);
     }
---
+++ Hotels/src/main/java/com/writingXml/WriteXML.java
@@ -0,0 +1,2 @@
+package com.writeXml;public class WriteXML {
+}
